e2bd82ec9255e57c94d0b2a239dd81b5356e5907
Yuriy Kuzin <yuriy.kuzin.pd@gmail.com>
2021-09-30 02:03:52 +0300

    added options for profile info

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,25 +1,158 @@
-import React, { useState } from 'react'
-import { useAuth } from '../contexts/AuthContext'
-import { useHistory } from 'react-router-dom'
+import React, { useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import DcContext from "../contexts/dc-context";
+import { useContext } from "react";
+
+//M-UI start
+import Alert from "@material-ui/lab/Alert";
+import AlertTitle from "@material-ui/lab/AlertTitle";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+//M-UI end
 
 export default function Account() {
-    const { currentUser, logout } = useAuth()
-    const [error, setError] = useState('')
-    const history = useHistory()
-    async function handleLogout() {
-        setError('')
-        try {
-            await logout()
-            history.push('/')
-        } catch (error) {
-            setError('Failed to logout')
-        }
+  const context = useContext(DcContext);
+  const nameRef = useRef();
+  const phoneRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const { currentUser, logout } = useAuth();
+  const [error, setError] = useState("");
+  const history = useHistory();
+
+  const client = context.clients.find((c) => c.email === currentUser.email);
+  const clientName = client && client.name;
+  const clientPhone = client && client.phone;
+
+  async function handleLogout() {
+    setError("");
+    try {
+      await logout();
+      history.push("/");
+    } catch (error) {
+      setError("Failed to logout");
     }
-    return (
-        <>
-            {error && <p>{error}</p>}
-            <p>{currentUser.email}</p>
-            <p><button onClick={handleLogout}>Log Out</button></p>
-        </>
-    )
+  }
+
+  //M-UI single line
+  const classes = useStyles();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      setError("");
+      setLoading(true);
+      if (client) {
+        client.name = nameRef.current.value;
+        client.phone = phoneRef.current.value;
+        context.updClient(client);
+      } else {
+        context.addClient({
+          name: nameRef.current.value,
+          email: currentUser.email,
+          phone: phoneRef.current.value,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      setError("Failed to update an account");
+    }
+    setLoading(false);
+  }
+
+  return (
+    <Container maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Profile info
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Full Name"
+            inputRef={nameRef}
+            defaultValue={clientName}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            label="Contact Phone"
+            inputRef={phoneRef}
+            defaultValue={clientPhone}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            disabled
+            fullWidth
+            label="Email"
+            defaultValue={currentUser.email}
+          />
+          {error && (
+            <Alert severity="error">
+              <AlertTitle>Error</AlertTitle>
+              <strong>{error}</strong>
+            </Alert>
+          )}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={loading}
+          >
+            Update info
+          </Button>
+        </form>
+        <Box mt={1}>
+          <Button
+            onClick={handleLogout}
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={loading}
+          >
+            Log out
+          </Button>
+        </Box>
+      </div>
+    </Container>
+  );
 }
