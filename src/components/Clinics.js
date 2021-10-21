7cbc7370e4f0c8882de2c185c529370a81b3d9ca
Yuriy Kuzin <yuriy.kuzin.pd@gmail.com>
2021-10-21 03:06:46 +0300

    added bg to clinics page

--- src/components/Clinics.js
+++ src/components/Clinics.js
@@ -2,12 +2,19 @@ import React, {useContext} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Grid, Typography } from "@material-ui/core";
 import Clinic from "./Clinic";
+import { CardMedia } from "@material-ui/core";
+import Bg from "../img/bg.jpg";
 
 import DcContext from "../contexts/dc-context";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  bg: {
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+    height: "100vh",
   },
 }));
 
@@ -16,6 +23,7 @@ export default function Clinics() {
   const context = useContext(DcContext);
 
   return (
+    <CardMedia image={Bg} className={classes.bg}>
     <div className={classes.root}>
       <Box m={2}>
         <Typography component="h1" variant="h5" align="center">
@@ -37,5 +45,6 @@ export default function Clinics() {
         </Grid>
       </Box>
     </div>
+    </CardMedia>
   );
 }
